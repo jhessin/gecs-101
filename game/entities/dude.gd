aec3c9bea44e694d84a30c9295c9db8d6e704871
Chris Sprance <chrissprance@gmail.com>
2025-10-07 10:12:55 -0400

    Update example

--- game/entities/dude.gd
+++ game/entities/dude.gd
@@ -3,9 +3,10 @@ extends Entity
 
 # Remember Entities are just containers and glue code
 
-# func on_ready() -> void:
-# 	# we probably want to sync the component transform to the node transform
-	#ECSUtils.sync_transform(self)
+func on_ready() -> void:
+	# we probably want to sync the component transform to the node transform
+	print('Look I am from dude', self.global_position)
+	ECSUtils.sync_transform(self)
 
 #func on_update(delta: float) -> void:
 #pass
